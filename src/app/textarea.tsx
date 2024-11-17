921c567ec2c282bf3d3e77d72ce3b2863de8fc1e
Mikoyzskie <rapidstrike13@gmail.com>
2024-11-17 20:31:45 +0800

    fix edit and autosize

--- src/app/textarea.tsx
+++ src/app/textarea.tsx
@@ -1,6 +1,6 @@
 "use client"
-
-import { ChangeEvent, useEffect, useRef, useState } from "react"
+import clsx from "clsx"
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react"
 
 export default function TextArea({ text }: { text: string }) {
 
@@ -15,11 +15,32 @@ export default function TextArea({ text }: { text: string }) {
   useEffect(() => {
     if (textAreaRef.current) {
       textAreaRef.current.style.height = "auto"
-      textAreaRef.current.style.height = `${focused ? textAreaRef.current.scrollHeight + 2 : textAreaRef.current.scrollHeight}px`;
+      textAreaRef.current.style.height = `${focused ? textAreaRef.current.scrollHeight + 1 : textAreaRef.current.scrollHeight}px`;
     }
   }, [val, focused]);
 
+  const handleEditComment = useCallback(() => {
+    if (textAreaRef.current) {
+      textAreaRef.current.focus();
+      const length = textAreaRef.current.value.length;
+      textAreaRef.current.setSelectionRange(length, length);
+    }
+
+  }, [])
+
   return (
-    <textarea value={val} onChange={handleOnChange} onFocus={() => { setFocused(true) }} onBlur={() => { setFocused(false) }} ref={textAreaRef} rows={1} className="text-[#67727E] resize-none outline-none focus:border-[#5357B6] focus:border focus:rounded-lg focus:px-6 focus:py-3" />
+    <>
+      <button onClick={handleEditComment} className={clsx("mb-5 w-fit border", focused && "pointer-events-none")}>Edit</button>
+      <textarea
+        value={val}
+        onChange={handleOnChange}
+        onMouseDown={(e) => e.preventDefault()}
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
+        ref={textAreaRef}
+        rows={1}
+        className="text-[#67727E] resize-none outline-none focus:border-[#5357B6] focus:border focus:rounded-lg focus:px-6 focus:py-3 overflow-hidden"
+      />
+    </>
   )
 }
